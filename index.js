c76524b534280618b624c33d015279a8863a8eda
leinue <597055914@qq.com>
2016-10-06 17:01:40 +0800

    update

--- index.js
+++ index.js
@@ -5,14 +5,32 @@ const PORT = 33033
   , PATH = '/var/www/gospely/socket'
 
 var deployServer = http.createServer(function(request, response) {
-  if (request.url.search(/deploy\/?$/i) > 0) {
+  console.log(request.url);
 
-    var commands = [
-      'cd ' + PATH,
-      'git pull'
-    ].join(' && ')
+  const cmds = {
+    '/api': 'docker exec gospel_api cd /var/www/api && git pull',
+    '/admin': 'docker exec gospel_admin cd /usr/share/nginx/html && git pull',
+    '/socket': 'cd /var/www/gospely/socket && git pull',
+    '/index': 'cd /var/www/gospely/index && git pull',
+    '/dash': 'docker exec gospel_dash cd /var/www/dash && git pull',
+    '/ide': '',
+    '/webhook': 'cd /var/www/gospely/webhook && git pull'
+  }
 
-    exec(commands, function(err, out, code) {
+  var inCMDs = false,
+      cmd = '';
+
+  for(var key in cmds) {
+    if(key == request.url) {
+      inCMDs = true;
+      cmd = cmds[key];
+      break;
+    }
+  }
+
+  if(inCMDs) {
+
+    exec(cmd, function(err, out, code) {
       if (err instanceof Error) {
         response.writeHead(500)
         response.end('Server Internal Error.')
@@ -21,17 +39,14 @@ var deployServer = http.createServer(function(request, response) {
       process.stderr.write(err)
       process.stdout.write(out)
       response.writeHead(200)
-      console.log('deploy done');
       response.end('Deploy Done.')
-
     })
 
-  } else {
-
+  }else {
     response.writeHead(404)
-    response.end('Not Found.')
-
+    response.end('Invalid Deploy Request')
   }
+
 })
 
 deployServer.listen(PORT)
