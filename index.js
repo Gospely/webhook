20391dc5ea73802124644c2751d85dfdebb24495
leinue <597055914@qq.com>
2016-10-06 17:18:00 +0800

    update

--- index.js
+++ index.js
@@ -8,11 +8,11 @@ console.log('listening port at: ' + PORT);
 var deployServer = http.createServer(function(request, response) {
 
   const cmds = {
-    '/api': 'docker exec gospel_api cd /var/www/api && git pull',
-    '/admin': 'docker exec gospel_admin cd /usr/share/nginx/html && git pull',
+    '/api': 'docker exec -d gospel_api cd /var/www/api && git pull',
+    '/admin': 'docker exec -d gospel_admin cd /usr/share/nginx/html && git pull',
     '/socket': 'cd /var/www/gospely/socket && git pull',
     '/index': 'cd /var/www/gospely/index && git pull',
-    '/dash': 'docker exec gospel_dash cd /var/www/dash && git pull',
+    '/dash': 'docker exec -d gospel_dash cd /var/www/dash && git pull',
     '/ide': '',
     '/webhook': 'cd /var/www/gospely/webhook && git pull'
   }
